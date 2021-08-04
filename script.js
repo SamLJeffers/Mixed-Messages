53d7056ffbc811282c032e897d48818ffa398b8a
unknown <88199486+SamLJeffers@users.noreply.github.com>
2021-08-04 10:57:02 +0100

    Mixed messages completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const positiveMessage = ["The sun is shinning, the weather is sweet", "Wake up positive, feel positive, have an awesome day", "You are braver than you believe, stronger that you seem and smarter than you think", "Winning doesn't always mean you finish first, winning means your doing better than you have done before"];
 
-const songQuote = ["Dont worry, be happy.", "What a wonderful World.", "Don't stop believing.", "Walking on sunshine.", "Get up, Stand up."];
+const songQuote = ["Don't worry, be happy.", "What a wonderful World.", "Don't stop believing.", "Walking on sunshine.", "Get up, Stand up."];
 
 const generateRandomMessage = () => {
     let positiveMessageIndex = Math.floor(Math.random()* positiveMessage.length);
